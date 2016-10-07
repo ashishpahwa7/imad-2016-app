b6ee8bc83d34955110943a6ceffb96633498cf81
Ashish Pahwa <ashishpahwa7@gmail.com>
2016-10-08 02:03:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -193,7 +193,7 @@ app.get('/submit-name/:name',function(req, res){
     
     var name = req.params.name;
     names.push(name);
-    res.send(JSON.stringfy(names));
+    res.send(JSON.stringify(names));
     
     
     
